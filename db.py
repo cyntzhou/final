d1e1c71495a380906e85746efba467bdd323361a
Cynthia Zhou <cyntzhou@gmail.com>
2015-01-06 20:49:02 -0500

    can now view classmates. also made their current teacher the default when Editing Schedule

--- db.py
+++ db.py
@@ -10,12 +10,23 @@ def new_user(user_params):
 #    user_params['last_login_at'] = None
 #    user_params['last_logout_at'] = None
     user_id = users.insert(user_params)
-    
     return user_id
 
 def find_user(criteria):
     user = users.find_one(criteria)
     return user
+
+def find_things(criteria):
+    things = users.find(criteria)
+    return things
+
+def find_things(criteria, attribute): #item is what you're searching for, e.g. username, password, etc.
+    things = users.find(criteria)
+    print things
+    l = []
+    for t in things:
+        l.append(str(t[attribute]))
+    return l #doesn't return the users; returns a list of the attributes of each user
 
 #gotta use $set or else update would repace the entry
 def update_user(criteria, changeset):
